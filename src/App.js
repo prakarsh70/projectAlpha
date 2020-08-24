bf5b7e77f2dd172c08a60ece440d057e10b19854
prakarsh <prakar.vastava@gmail.com>
2020-08-24 19:09:09 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Alpha from './alpha.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={Alpha} className="App-logo" alt="logo" />
+        <p>
+          ProjectAlpha
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
