fb6eaff107733850e33716c9f518fe3287ca1cb2
prakarsh <prakar.vastava@gmail.com>
2020-08-30 17:35:38 +0530

    Added glitch animation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
-import Alpha from './alpha.svg';
-import './App.css';
+import React from "react";
+import Alpha from "./alpha.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={Alpha} className="App-logo" alt="logo" />
-        <p>
-          ProjectAlpha 
-        </p>
-        
+        <div class="container">
+          <div class="glitch" data-text="ProjectAlpha">
+          ProjectAlpha
+          </div>
+          <div class="glow">ProjectAlpha</div>
+          <p class="subtitle">this is ProjectAlpha bitches!</p>
+        </div>
+        <div class="scanlines"></div>
       </header>
     </div>
   );
