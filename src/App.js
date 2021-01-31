28a5754cd5b88a6a7a21f8354363bebd52a8792c
prakarsh <prakar.vastava@gmail.com>
2021-01-31 23:55:17 +0530

    deleted unwanted code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from "react";
-import Alpha from "./alpha.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={Alpha} className="App-logo" alt="logo" />
-        <div class="container">
-          <div class="glitch" data-text="ProjectAlpha">
-          ProjectAlpha
-          </div>
-          <div class="glow">ProjectAlpha</div>
-          <p class="subtitle">this is ProjectAlpha bitches!</p>
-        </div>
-        <div class="scanlines"></div>
-      </header>
+      App
     </div>
   );
 }
